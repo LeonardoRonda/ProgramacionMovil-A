7a6f46cd1bc42ec04d708f32282ca683291d84df
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-09-13 22:32:30 -0500

    Agrega los compasable ContadorRoto y ContadorConRemember, dentro se implementan buttons para el cambio de estado.

--- semana4/app/src/main/java/com/ronda/lab04/MainActivity.kt
+++ semana4/app/src/main/java/com/ronda/lab04/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,6 +16,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ronda.lab04.ui.theme.Lab04Theme
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +33,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        ContadorConRemember() // O ContadorRoto(), aunque se recomienda usar este
+                    }
                 }
             }
         }
@@ -31,17 +43,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab04Theme {
-        Greeting("Android")
+fun ContadorRoto() {
+    var contador = 0
+    Column {
+        Text("Contador: $contador")
+        Button(onClick = { contador++ }) {
+            Text("Incrementar")
+        }
     }
 }
+
+@Composable
+fun ContadorConRemember(){
+    var contador by remember {mutableStateOf(0)}
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text="Contador: $contador",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier=Modifier.height(8.dp))
+        Button(onClick = {contador++}) {
+            Text("Incrementar")
+        }
+    }
+}
+
+
