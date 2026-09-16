38d5fb5666317d76544ac8ccdf11d5abe8c0e42b
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-09-16 14:54:39 -0500

    Formulario agrega productos a la lista observable, el formulario fue redisenado como se solicito.

--- Lab04CarritoTecsup/app/src/main/java/com/ronda/lab04carritotecsup/Producto.kt
+++ Lab04CarritoTecsup/app/src/main/java/com/ronda/lab04carritotecsup/Producto.kt
@@ -1,6 +1,20 @@
 package com.ronda.lab04carritotecsup
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,15 +22,87 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 data class Producto (val nombre: String, val precio: Double, val cantidad: Int)
 
 @Composable
-fun PantallaCarrito(){
+fun PantallaCarrito(modifier: Modifier) {
     var nombre by remember { mutableStateOf("") }
     var precio by remember { mutableStateOf("") }
     var cantidad by remember { mutableStateOf("") }
     val productos = remember { mutableStateListOf<Producto>() }
 
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Box (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(75.dp)
+                .background(Color(0xFF6332B4))
+                .padding(10.dp),
+            contentAlignment = Alignment.CenterStart
+        ){
+            Text(
+                text = "Mi carrito TECSUP",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = { nombre = it },
+            label = { Text("Nombre del producto: ") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            OutlinedTextField(
+                value = precio,
+                onValueChange = { precio = it },
+                label = { Text("Precio (S/.): ") },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            OutlinedTextField(
+                value = cantidad,
+                onValueChange = { cantidad = it },
+                label = { Text("Cantidad: ") },
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Button(
+            onClick = {
+                val precioNum = precio.toDoubleOrNull() ?: 0.0
+                val cantidadNum = cantidad.toIntOrNull() ?: 0
+                if (nombre.isNotBlank() && precioNum > 0 && cantidadNum > 0) {
+                    productos.add(Producto(nombre, precioNum, cantidadNum))
+
+                    nombre = ""
+                    precio = ""
+                    cantidad = ""
+                }
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF6332B4),
+                contentColor = Color.White,
+                disabledContainerColor = Color.Gray,
+                disabledContentColor = Color.LightGray
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) { Text("AGREGAR") }
+        Text(
+            text = "Producto: ${productos.size}"
+        )
+
+    }
 }
