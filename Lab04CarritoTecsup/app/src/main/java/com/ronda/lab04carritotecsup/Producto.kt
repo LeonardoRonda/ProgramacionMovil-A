f5f0eb7eebf94145f1b02e96323dbdc302d205c5
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-09-16 13:51:49 -0500

    Implementa el Composable PantallaCarrito y sus varibales usadno var, remember y mutableStateOf y mutableStateListOf

--- Lab04CarritoTecsup/app/src/main/java/com/ronda/lab04carritotecsup/Producto.kt
+++ Lab04CarritoTecsup/app/src/main/java/com/ronda/lab04carritotecsup/Producto.kt
@@ -1,3 +1,22 @@
 package com.ronda.lab04carritotecsup
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+
 data class Producto (val nombre: String, val precio: Double, val cantidad: Int)
+
+@Composable
+fun PantallaCarrito(){
+    var nombre by remember { mutableStateOf("") }
+    var precio by remember { mutableStateOf("") }
+    var cantidad by remember { mutableStateOf("") }
+    val productos = remember { mutableStateListOf<Producto>() }
+
+}
