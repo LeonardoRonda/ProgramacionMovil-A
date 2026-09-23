5d857ec1550340b2a60c8277729d23b3cabf0d0f
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-09-23 02:30:30 -0500

    Crea el MainActivity.kt y emplea NavHost

--- semana5/app/src/main/java/com/ronda/navlab/MainActivity.kt
+++ semana5/app/src/main/java/com/ronda/navlab/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.ronda.navlab
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ronda.navlab.ui.theme.NavLabTheme
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.ronda.navlab.navigation.Screen
+import com.ronda.navlab.screens.DetailScreen
+import com.ronda.navlab.screens.HomeScreen
+import com.ronda.navlab.screens.ListScreen
+import com.ronda.navlab.screens.ProfileScreen
+import com.ronda.navlab.ui.theme.NavLabTheme // O el nombre del tema de tu app
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,29 +23,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavLabTheme {
-        Greeting("Android")
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument(name = "itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
     }
 }
