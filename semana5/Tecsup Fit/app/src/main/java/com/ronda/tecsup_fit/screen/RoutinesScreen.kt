5ab69a125c7c24b5d41ec069893460d6b2235138
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-09-23 06:24:32 -0500

    Crea los archivos RoutinesScreen.kt,ListScreen,DetailScreen, aqui se guardaran las rutinas.

--- semana5/Tecsup Fit/app/src/main/java/com/ronda/tecsup_fit/screen/RoutinesScreen.kt
+++ semana5/Tecsup Fit/app/src/main/java/com/ronda/tecsup_fit/screen/RoutinesScreen.kt
@@ -1,2 +1,38 @@
 package com.ronda.tecsup_fit.screen
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun RoutinesScreen(
+    navController: NavController
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
+
+        Text(
+            "Rutinas",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Text("🏋 Yoga funcional")
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text("💪 Cross Training")
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text("🏃 Spinning")
+    }
+}
