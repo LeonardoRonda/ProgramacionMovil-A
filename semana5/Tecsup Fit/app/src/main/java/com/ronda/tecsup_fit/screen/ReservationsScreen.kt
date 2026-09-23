ef73f4a3b960e4bc118cd8f20221676ef6432a60
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-09-23 06:25:33 -0500

    Crea el contenido del ReservationsScreen.kt, se usa navcontroller y se estructura la informacion usando un column, se importa la clase Screen de la carpeta navigation

--- semana5/Tecsup Fit/app/src/main/java/com/ronda/tecsup_fit/screen/ReservationsScreen.kt
+++ semana5/Tecsup Fit/app/src/main/java/com/ronda/tecsup_fit/screen/ReservationsScreen.kt
@@ -1,2 +1,57 @@
 package com.ronda.tecsup_fit.screen
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun ReservationsScreen(
+    navController: NavController
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+        Text(
+            "Mis reservas",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        LazyColumn {
+
+            items(clases.take(2)) { clase ->
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 5.dp)
+                ) {
+
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+
+                        Text(
+                            clase.nombre,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+
+                        Text("${clase.hora} • ${clase.sala}")
+
+                        Text("Confirmada")
+                    }
+                }
+            }
+        }
+    }
+}
