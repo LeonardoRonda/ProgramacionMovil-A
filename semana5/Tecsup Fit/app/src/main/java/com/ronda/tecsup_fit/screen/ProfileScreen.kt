0de392ca5015a1b908c76a3b6c3d69c293ecc3bd
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-09-23 06:30:14 -0500

    Crea el contenido del profileScreen, Aq[i se uso column, row y card, para organizar la informacion del usuario.

--- semana5/Tecsup Fit/app/src/main/java/com/ronda/tecsup_fit/screen/ProfileScreen.kt
+++ semana5/Tecsup Fit/app/src/main/java/com/ronda/tecsup_fit/screen/ProfileScreen.kt
@@ -1,2 +1,69 @@
 package com.ronda.tecsup_fit.screen
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Mi perfil",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = "DR",
+            style = MaterialTheme.typography.displaySmall
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "Diego Ramos",
+            style = MaterialTheme.typography.titleLarge
+        )
+
+        Text("Plan Premium")
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+
+            Card {
+                Column(
+                    modifier = Modifier.padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("14")
+                    Text("Clases")
+                }
+            }
+
+            Card {
+                Column(
+                    modifier = Modifier.padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("3")
+                    Text("Rachas")
+                }
+            }
+        }
+    }
+}
